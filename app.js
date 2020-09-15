f0bc41fa677ec2331ca64265e377633d4f0d0bfb
Mart-Jan Koedam <0962472@hr.nl>
2020-09-15 16:27:43 +0200

    user credentials are saved in Homey and are used to log in to the crownstone cloud succesfully

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 'use strict';
 
 const Homey = require('homey');
+let cloudLib = require("crownstone-cloud")
+let cloud = new cloudLib.CrownstoneCloud();
+
+async function login(CrownstoneUsername, CrownstonePassword){
+	await cloud.login(CrownstoneUsername, CrownstonePassword);
+	console.log(await cloud.me().data());
+
+	let idFilter = '5f4f69a6145e6e0004f60f0d';
+	let dataFromId = await cloud.crownstone(idFilter).data();
+	let leds = cloud.crownstone('5f4f69a6145e6e0004f60f0d');
+	await leds.turnOn();
+	sleep(5000);
+	await leds.turnOff();
+}
+
+function sleep(milliseconds) {
+	const date = Date.now();
+	let currentDate = null;
+	do {
+		currentDate = Date.now();
+	} while (currentDate - date < milliseconds);
+}
 
 class MyApp extends Homey.App {
 	
 	onInit() {
-		this.log('The Crownstone app beta from M-J is running...');
+		this.log(`App ${Homey.app.manifest.name.en} is running...`);
+
+		this.email = Homey.ManagerSettings.get("email");
+		this.password = Homey.ManagerSettings.get("password");
+
+		login(this.email, this.password).catch((e) => { console.log("There was a problem running this code:", e); });
 	}
-	
+
 }
 
 module.exports = MyApp;
