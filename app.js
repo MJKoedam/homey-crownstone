096c58a3ae4582fb49c5aa088defb39768ae9f20
MJKoedam <martjankoedam42@gmail.com>
2020-09-21 14:36:18 +0200

    Added function to make new connection to the cloud when user changes credentials

--- app.js
+++ app.js
@@ -4,24 +4,16 @@ const Homey = require('homey');
 let cloudLib = require("crownstone-cloud")
 let cloud = new cloudLib.CrownstoneCloud();
 
-async function login(CrownstoneUsername, CrownstonePassword){
-	await cloud.login(CrownstoneUsername, CrownstonePassword);
+async function login(email, password){
+	await cloud.login(email, password);
 	console.log(await cloud.me().data());
 
-	let idFilter = '5f4f69a6145e6e0004f60f0d';
-	let dataFromId = await cloud.crownstone(idFilter).data();
-	let light = cloud.crownstone('5f4f69a6145e6e0004f60f0d');
-	await light.turnOn();
-	sleep(5000);
-	await light.turnOff();
-}
-
-function sleep(milliseconds) {
-	const date = Date.now();
-	let currentDate = null;
-	do {
-		currentDate = Date.now();
-	} while (currentDate - date < milliseconds);
+	// let idFilter = '5f4f69a6145e6e0004f60f0d';
+	// let dataFromId = await cloud.crownstone(idFilter).data();
+	// let light = cloud.crownstone('5f4f69a6145e6e0004f60f0d');
+	// await light.turnOn();
+	// sleep(5000);
+	// await light.turnOff();
 }
 
 class MyApp extends Homey.App {
@@ -33,13 +25,23 @@ class MyApp extends Homey.App {
 		this.password = Homey.ManagerSettings.get("password");
 
 		Homey.ManagerSettings.on('set', function(){
-			this.log("App Settings Were Changed!");
-			//put code in here that runs when userdata changed
+			this.log("App Settings Were Changed! New credentials!");
+			this.email = Homey.ManagerSettings.get("email");
+			this.password = Homey.ManagerSettings.get("password");
+			login(this.email, this.password).catch((e) => { console.log("There was a problem running this code:", e); });
 		});
 
 		login(this.email, this.password).catch((e) => { console.log("There was a problem running this code:", e); });
 	}
 
+}
+
+function sleep(milliseconds) {
+	const date = Date.now();
+	let currentDate = null;
+	do {
+		currentDate = Date.now();
+	} while (currentDate - date < milliseconds);
 }
 
 module.exports = MyApp;
