f6311ae647782462f24015696da12e570c305a97
MJKoedam <martjankoedam42@gmail.com>
2020-10-05 19:13:51 +0200

    added the ability to switch the state of the Crownstone

--- drivers/crownstone/device.js
+++ drivers/crownstone/device.js
@@ -1,24 +1,41 @@
 'use strict';
 
 const Homey = require('homey');
+let accessToken;
 
-class MyDevice extends Homey.Device {
+class CrownstoneDevice extends Homey.Device {
 	
 	onInit() {
 		this.log('MyDevice has been inited');
 		this.log('Name:', this.getName());
 		this.log('Class:', this.getClass());
-
+		console.log(this.getData().id);
 		this.registerCapabilityListener('onoff', this.onCapabilityOnoff.bind(this));
+		this.cloud = Homey.app.getCloud();
+		accessToken = Homey.app.getUserToken(function(token){accessToken = token;});
 	}
 
+	//Switch the Crownstone
 	async onCapabilityOnoff( value, opts ) {
+		this.log("Change state to " + value);
+		if(value){
+			await this.cloud.crownstone(this.getData().id).turnOn()
+		} else if(!value){
+			await this.cloud.crownstone(this.getData().id).turnOff()
+		}
+		//todo: handle errors
+	}
 
-		// ... set value to real device, e.g.
-		// await setMyDeviceState({ on: value });
+	// this method is called when the Device is added
+	onAdded() {
+		this.log(this.getName() + " has been added.");
+	}
 
-		// or, throw an error
-		// throw new Error('Switching the device failed!');
+	// this method is called when the Device is deleted
+	onDeleted() {
+		this.log(this.getName() + " has been deleted.");
 	}
 
 }
+
+module.exports = CrownstoneDevice;
